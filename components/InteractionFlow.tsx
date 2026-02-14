1b42193dcad233a50dddbb69058996fbdea5b62a
Haikal Rizky <haikalrizky.21@gmail.com>
2026-02-15 03:27:13 +0700

    update text

--- components/InteractionFlow.tsx
+++ components/InteractionFlow.tsx
@@ -340,7 +340,9 @@ const LoveMeterStep = ({ onComplete }: { onComplete: () => void }) => {
 
 // --- Step 4: Typewriter ---
 const TypewriterStep = ({ onComplete }: { onComplete: () => void }) => {
-    const text = "Happy Valentine! My Lovely, Nuril Hasmi❤️";
+    const text = "Happy Valentine!";
+    const text = "My Lovely,"
+    const text = "Nuril Hasmi❤️"
     const [displayedText, setDisplayedText] = useState("");
     const [isDeleting, setIsDeleting] = useState(false);
 
